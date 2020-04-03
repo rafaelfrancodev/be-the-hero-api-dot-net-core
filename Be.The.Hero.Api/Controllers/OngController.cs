8375d7ce1adc8733a014d131ca47a536a7e7d03c
Rafael Franco <rafael.apfsantos@outlook.com>
2020-04-03 19:19:32 -0300

    Add Ong Controller

---
+++ Be.The.Hero.Api/Controllers/OngController.cs
@@ -0,0 +1,42 @@
+﻿using Be.The.Hero.Api.Interfaces.Services;
+using Be.The.Hero.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Be.The.Hero.Api.Controllers
+{
+    [Route("ongs")]
+    [ApiController]
+    public class OngController : ControllerBase
+    {
+        private readonly IOngService _ongService;
+
+        public OngController(IOngService ongService)
+        {
+            _ongService = ongService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Ong>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> GetAsync()
+        {
+            var resultado = await _ongService.SelectAllAsync();
+            return Ok(resultado);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Ong), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> PostAsync([FromBody] Ong input)
+        {
+            var resultado = await _ongService.InsertAsync(input);
+            return Ok(resultado);
+        }
+    }
+}
--- Be.The.Hero.Api/Startup.cs
+++ Be.The.Hero.Api/Startup.cs
@@ -11,6 +11,12 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System.IO;
+using System.Reflection;
+using System;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Be.The.Hero.Api
 {
@@ -25,25 +31,18 @@ namespace Be.The.Hero.Api
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(options =>
-            {
-                options.AddPolicy("CorsPolicy", builder => builder
-                    .AllowAnyOrigin()
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowCredentials());
-            });
+            AddCors(services);
+            AddSwagger(services);
+            AddMVC(services);
+            AddOptions(services);          
 
             var connection = Configuration["SqliteConnectionString"];
             services.AddDbContext<BeTheHeroSQLiteContext>(options =>
                 options.UseSqlite(connection)
             );
-            // Add framework services.
-            services.AddMvc();
+
             services.AddScoped<IOngService, OngService>();
             services.AddScoped<IOngRepository, OngRepository>();
-
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,14 +55,54 @@ namespace Be.The.Hero.Api
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+            app.UseCors("CorsPolicy");
+            app.UseAuthentication();
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(s =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+                s.SwaggerEndpoint("/swagger/v1/swagger.json", "Be The Hero - API");
+            });            
+
+            var option = new RewriteOptions();
+            option.AddRedirect("^$", "swagger");
+            app.UseRewriter(option);
 
         }
+        private void AddCors(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+        }
+        private void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Be The Hero - API", Version = "v1" });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                if (File.Exists(xmlPath))
+                {
+                    c.IncludeXmlComments(xmlPath);
+                }
+            });
+        }
+
+        private void AddMVC(IServiceCollection services)
+        {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+        }
+
+        private void AddOptions(IServiceCollection services)
+        {
+            services.AddOptions();
+        }
+
     }
 }
